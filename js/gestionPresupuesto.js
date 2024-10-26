27d59d80f11cbfec772616100da40dc208ccac0d
ASENSIO EMILIO REBOLLO GÓMEZ <aserebgom@alu.edu.gva.es>
2024-10-26 18:15:08 +0200

    funcion actualizarValor

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -38,6 +38,7 @@ function CrearGasto(descripcion,valor) {
         else  {  
                 this.valor =0;
         }
+        
        
     //Funcion MostrarGasto
         this.mostrarGasto = function() {
@@ -52,14 +53,12 @@ function CrearGasto(descripcion,valor) {
             this.descripcion = "";   
         }
         this.actualizarValor = function(nuevoValor) {
-            if (typeof valor === "number" && valor >=0){
-                this.valor = nuevoValor;
-            }
-            else
-            this.valor = "";   
+            if (typeof nuevoValor === "number" && nuevoValor >= 0) {
+                this.valor =nuevoValor; 
+            }       
         }
+        
     }
-
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
 // Las funciones y objetos deben tener los nombres que se indican en el enunciado
 // Si al obtener el código de una práctica se genera un conflicto, por favor incluye todo el código que aparece aquí debajo
