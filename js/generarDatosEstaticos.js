f5d9569b25c1f9b03513a4e874ab239d8c669366
ASENSIO EMILIO REBOLLO GÓMEZ <aserebgom@alu.edu.gva.es>
2024-11-17 20:10:38 +0100

    creo ficheros js y enlace para HTML

---
+++ js/generarDatosEstaticos.js
@@ -0,0 +1,5 @@
+<script type="module"> 
+    import * as gestionPresupuesto from './js/gestionPresupuesto.js';
+    import * as gestionPresupuestoWeb from './js/gestionPresupuestoWeb.js';
+   
+</script>
